e3a6cd495630ae15add6a50a27be0b8a99420c58
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2021-07-23 12:37:07 +0200

    Updated with new nav pills

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ function NavPill(props) {
     <NavLink
       exact
       to={props.to}
-      className="flex-1 px-8 pt-2 pb-safeBottom flex flex-col bg-white border-gray-200 border-t text-sm leading-8 text-gray-400 text-center hover:bg-gray-100 transition-all ease-in-out duration-200 dark:bg-black dark:border-gray-700 dark:hover:bg-gray-800"
+      className="flex-1 px-2 pt-2 pb-safeBottom flex flex-col bg-white border-gray-200 border-t text-sm leading-8 text-gray-400 text-center hover:bg-gray-100 transition-all ease-in-out duration-200 dark:bg-black dark:border-gray-700 dark:hover:bg-gray-800"
       activeClassName="text-primary"
     >
       {props.children}
@@ -19,6 +19,9 @@ function Nav() {
     <nav className="sticky z-50 bottom-0 flex justify-center self-end w-full">
       <NavPill to="/">
         <i className="ri-home-line text-base"></i>Hem
+      </NavPill>
+      <NavPill to="/galleri">
+        <i className="ri-gallery-line text-base"></i>Galleri
       </NavPill>
       <div
         style={{ width: "120px" }}
@@ -35,6 +38,9 @@ function Nav() {
       </div>
       <NavPill to="/profil">
         <i className="ri-user-line text-base"></i>Profil
+      </NavPill>
+      <NavPill to="/info">
+        <i className="ri-information-line text-base"></i>Info
       </NavPill>
     </nav>
   );
