1476559b4987b8057b1755375a58b790437cfd63
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2021-08-07 19:21:19 +0200

    Fixed headings on gallery page

--- src/components/gallery/GalleryGrid.js
+++ src/components/gallery/GalleryGrid.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function GalleryGrid(props) {
-    return (
-        <section className="relative py-20 px-8 lg:px-20">
-            <h2 className="text-black dark:text-white text-title1">{props.galleryTitle}</h2>
-            <div className="max-w-screen-xl -mx-8 lg:mx-auto grid grid-cols-3 gap-2 lg:grid-cols-5">
-                {props.children}
-            </div>
-        </section>
-    )
+  return (
+    <section className="relative py-20 px-8 lg:px-20">
+      <div className="sticky z-10 top-0 -mx-8 xl:-mx-20 pt-6 pb-4 mb-8 bg-white dark:bg-ultraBlack transition-all ease-linear duration-100">
+        <div className="w-full max-w-screen-xl mx-auto px-8">
+          <h2 className="text-black dark:text-white text-title1">
+            {props.galleryTitle}
+          </h2>
+        </div>
+      </div>
+
+      <div className="max-w-screen-xl -mx-8 lg:mx-auto grid grid-cols-3 gap-2 lg:grid-cols-5">
+        {props.children}
+      </div>
+    </section>
+  );
 }
