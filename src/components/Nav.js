0ebfeb597abe009775f1c1f136ed597ecb9a8a33
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2021-08-07 19:17:22 +0200

    Fixed issue with nav background image

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ function NavPill(props) {
     <NavLink
       exact
       to={props.to}
-      className="flex-1 px-2 pt-2 pb-safeBottom flex flex-col bg-white border-gray-200 border-t text-sm leading-8 text-gray-400 text-center hover:bg-gray-100 transition-all ease-in-out duration-200 dark:bg-black dark:border-gray-700 dark:hover:bg-gray-800"
+      className="flex-1 px-2 pt-2 pb-safeBottom flex flex-col text-sm leading-8 text-gray-400 text-center hover:bg-gray-100 transition-all ease-in-out duration-200 dark:hover:bg-gray-800"
       activeClassName="text-primary"
     >
       {props.children}
@@ -16,7 +16,7 @@ function NavPill(props) {
 
 function Nav() {
   return (
-    <nav className="fixed z-50 bottom-0 left-0 flex justify-center w-full">
+    <nav className="fixed z-50 bottom-0 left-0 flex justify-center w-full bg-white border-gray-200 border-t dark:bg-black dark:border-gray-700">
       <NavPill to="/">
         <i className="ri-home-line text-base"></i>Hem
       </NavPill>
@@ -24,8 +24,8 @@ function Nav() {
         <i className="ri-gallery-line text-base"></i>Galleri
       </NavPill>
       <div
-        style={{ width: "80px", marginTop: "-25px" }}
-        className="relative bg-menu-bg dark:bg-menu-dark bg-no-repeat bg-center-top bg-cover"
+        style={{ width: "80px", marginTop: "-16px", backgroundSize: "80px auto" }}
+        className="relative bg-menu-bg dark:bg-menu-dark bg-no-repeat bg-center-top"
       >
         <NavLink
           exact
