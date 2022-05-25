b746e8ba8b5e9eaf85efddee1dd652513dbcbfdc
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-05-25 20:52:01 +0200

    Fixed broken link

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -25,7 +25,7 @@ function Weather() {
           // if (!d.getElementById(id)) {
           js = d.createElement(s);
           js.id = id;
-          js.src = "https:weatherwidget.io/js/widget.min.js";
+          js.src = "https://weatherwidget.io/js/widget.min.js";
           fjs.parentNode.insertBefore(js, fjs);
           // }
         })(document, "script", "weatherwidget-io-js")
