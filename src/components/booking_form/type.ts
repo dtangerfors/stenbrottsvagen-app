349f8dedfa3c46a25d201d33fab36a0bc47fce1c
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-07-20 22:19:55 +0200

    Moved type to own file

---
+++ src/components/booking_form/type.ts
@@ -0,0 +1,11 @@
+export type BookingData = {
+   bookingName: string;
+   bookingGuests: number;
+   bookingArrival: string;
+   bookingDeparture: string;
+   bookingMessage: string;
+   bookingUserID: string;
+   rooms: any[];
+   createdAt: number;
+   updatedAt: number;
+ };
