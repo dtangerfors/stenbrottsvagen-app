0e889411b06a3b33f2f7a949dbf2829addcb7ff0
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-07-21 22:51:03 +0200

    Added context and new card

--- src/components/ViewAllBookings.js
+++ src/components/ViewAllBookings.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { onValue } from "firebase/database";
 import bookingDB from "../firebase/bookingDb";
-import Card from "./CardUpdated";
+import Card from "./BookingCard";
 import { onDataChange } from "../firebase/index";
+import { UserContext } from "../user-context";
 
 function ViewAllBookings(props) {
   const [bookings, setBookings] = useState([]);
+  const loggedInUser = useContext(UserContext);
 
   useEffect(() => {
     onValue(bookingDB.getAllBookings(), (snapshot) => {
@@ -21,6 +23,8 @@ function ViewAllBookings(props) {
     };
   },[]);
 
+
+
   const sortedBookings = bookings.sort(function (a, b) {
     var dateA = new Date(a.bookingArrival),
       dateB = new Date(b.bookingArrival);
@@ -29,6 +33,7 @@ function ViewAllBookings(props) {
 
   const noFutureBookings = (booking) =>
     Date.parse(booking.bookingDeparture) <= Date.now();
+
 
   if (!bookings || bookings.every(noFutureBookings)) {
     return (
@@ -39,7 +44,10 @@ function ViewAllBookings(props) {
   } else {
     return sortedBookings
       .filter((booking) => Date.parse(booking.bookingDeparture) >= Date.now())
-      .map((booking, index) => <Card key={index} booking={booking} />);
+      .map((booking, index) => {
+        let isLoggedIn = loggedInUser.uid === booking.bookingUserID ? true : false;
+      return (<Card userIsLoggedIn={isLoggedIn} key={index} booking={booking} isUpdatingBooking={props.isUpdatingBooking} />)
+    });
   }
 }
 
