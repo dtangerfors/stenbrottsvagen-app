a6aa9272a713fe1d9ec92d31ebadfbfedc3dd2d4
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-07-22 15:09:40 +0200

    Fixed notification not showing properly

--- src/components/Complete.js
+++ src/components/Complete.js
@@ -13,12 +13,12 @@ export default function Complete(props) {
   };
 
   return (
-    <div className="fixed top-8 px-8 w-full">
-      <div className="max-w-screen-sm px-8 mx-auto flex justify-start items-center bg-white rounded-sm shadow-xl">
-        <div className="mr-12">
+    <div className="fixed top-0 z-50 px-8 w-full bg-gradient-to-b from-black/10 to-black/0 py-10">
+      <div className="max-w-screen-sm pr-8 pl-4 mx-auto flex justify-start items-center bg-white rounded-sm shadow-xl">
+        <div className="mr-4">
           <Lottie options={defaultOptions} height={60} width={60} />
         </div>
-        <p className="text-base">{props.message}</p>
+        <p className="text-base text-black">{props.message}</p>
       </div>
     </div>
   );
