925029ab29e899eb0a455e04cec8c4920c977a20
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-07-24 20:22:58 +0200

    Changed design and converted to TS

---
+++ src/components/Login.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { PrimaryButton } from "./buttons";
+
+export default function Login(props: any) {
+  return (
+    <div className="w-screen h-screen grid grid-rows-2">
+      <div className="bg-cover-bg bg-cover bg-center"></div>
+      <div className="bg-primaryLight px-8">
+        <div className="relative max-w-screen-sm -top-20 mx-auto rounded-sm p-12 bg-white text-center">
+          <header className="mb-10">
+            <h1 className="font-title font-semibold text-title1">
+              Stenbrottsvägen 3
+            </h1>
+            <p className="text-base text-gray-700">
+              Logga in för att boka och se information
+            </p>
+          </header>
+
+          <PrimaryButton
+            onClick={props.login}
+            text={"Logga in med Google"}
+            actionText={
+              "Klicka här för att logga in till portalen"
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
