1bf03eb8c4a85cac1961afb724bf34a27914564f
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-08-09 23:31:35 +0200

    Removed process env from property

--- src/components/gallery/Image.js
+++ src/components/gallery/Image.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Image({image}) {
     return (
-    <a className="relative" href={process.env.GALLERY_URL+image}>
+    <a className="relative" href={image}>
         <figure className="relative pt-100">
             <img src={image} alt="" className="absolute w-full h-full top-1/2 transform -translate-y-1/2 object-cover" />
         </figure>
